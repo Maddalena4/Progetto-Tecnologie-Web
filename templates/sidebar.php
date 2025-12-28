5364bf246152c22189cedaef90e113d81f64e7e7
Maddalena Prandini <maddalena.prandini@studio.unibo.it>
2025-12-28 12:24:27 +0100

    updatin admin bar

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,49 +1,34 @@
-<div class="offcanvas offcanvas-start sidebar-bg" tabindex="-1" id="sidebar">
-    <div class="offcanvas-header">
-        <h5 class="offcanvas-title">Menu</h5>
-        <button type="button"
-                class="btn-close btn-close-white"
-                data-bs-dismiss="offcanvas"
-                aria-label="Chiudi"></button>
-    </div>
+<ul class="nav flex-column gap-3">
 
-    <div class="offcanvas-body">
-        <ul class="nav flex-column gap-3">
+    <li class="nav-item">
+        <div class="sidebar-link nav-link d-flex align-items-center text-white">
+            <i class="bi bi-person-circle me-2"></i>
+            <span><?= htmlspecialchars($_SESSION['email']); ?></span>
+        </div>
+    </li>
 
-            <li class="nav-item">
-                <div class="sidebar-link nav-link d-flex align-items-center text-white">
-                    <i class="bi bi-person-circle me-2"></i>
-                    <span>
-                        <?php echo htmlspecialchars($_SESSION['email']); ?>
-                    </span>
-                </div>
-            </li>
+    <li class="nav-item">
+        <a class="sidebar-link nav-link" href="admin.php?action=home">
+            <i class="bi bi-house-fill me-2"></i>Home
+        </a>
+    </li>
 
+    <li class="nav-item">
+        <a class="sidebar-link nav-link" href="admin.php?action=corsi">
+            <i class="bi bi-journal-bookmark-fill me-2"></i>Corsi
+        </a>
+    </li>
 
-            <li class="nav-item">
-                <a class="sidebar-link nav-link" href="admin.php?action=home">
-                    <i class="bi bi-house-fill me-2"></i>Home
-                </a>
-            </li>
+    <li class="nav-item">
+        <a class="sidebar-link nav-link" href="admin.php?action=facolta">
+            <i class="bi bi-mortarboard-fill me-2"></i>Facoltà
+        </a>
+    </li>
 
-            <li class="nav-item">
-                <a class="sidebar-link nav-link" href="admin.php?action=corsi">
-                    <i class="bi bi-journal-bookmark-fill me-2"></i>Corsi
-                </a>
-            </li>
+    <li class="nav-item mt-5">
+        <a class="sidebar-link nav-link text-white" href="index.php?logout=1">
+            <i class="bi bi-box-arrow-left me-2"></i>Esci
+        </a>
+    </li>
 
-            <li class="nav-item">
-                <a class="sidebar-link nav-link" href="admin.php?action=facolta">
-                    <i class="bi bi-mortarboard-fill me-2"></i>Facoltà
-                </a>
-            </li>
-
-            <li class="nav-item mt-5">
-                <a class="sidebar-link nav-link text-white" href="index.php?logout=1">
-                    <i class="bi bi-box-arrow-left me-2"></i>Esci
-                </a>
-            </li>
-        </ul>
-        <hr class="text-white my-3">
-    </div>
-</div>
+</ul>
