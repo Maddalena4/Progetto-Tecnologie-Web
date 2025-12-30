a6c0e1eb3e0c70a8c1da97931a177bc6db3398a9
Maddalena Prandini <maddalena.prandini@studio.unibo.it>
2025-12-30 17:02:33 +0100

    third draft of create

--- admin_facolta_controller.php
+++ admin_facolta_controller.php
@@ -1,33 +1,45 @@
-<?php 
+<?php
 session_start();
-
 require_once "bootstrap.php";
 
-//Controllo login
-if(!isUserLoggedIn()) {
-    header("Location:login.php");
-    exit;
-}
-if(getUserRole() !== 'admin') {
-    header("Location:index.php");
+// Sicurezza
+if (!isUserLoggedIn() || getUserRole() !== 'admin') {
+    header("Location: index.php");
     exit;
 }
 
-//controllo pararmetro
-if (!isset($_POST['idfacolta']) || !is_numeric($_POST['idfacolta'])) {
-    header("Location: admin.php?action=facolta&error=invalid");
+// Crea Facoltà
+if (isset($_POST['action']) && $_POST['action'] === 'create') {
+
+    if (empty($_POST['nome_facolta']) || empty($_POST['tipologia'])) {
+        header("Location: admin.php?action=crea_facolta&error=missing");
+        exit;
+    }
+
+    $nome = $_POST['nome_facolta'];
+    $tipologia = $_POST['tipologia'];
+
+    if ($dbh->addFacolta($nome, $tipologia)) {
+        header("Location: admin.php?action=facolta&success=created");
+    } else {
+        header("Location: admin.php?action=crea_facolta&error=db");
+    }
     exit;
 }
 
-$idFacolta = intval($_POST['idfacolta']);
+// Elimina Facoltà
+if (isset($_POST['idfacolta']) && is_numeric($_POST['idfacolta'])) {
 
-//eliminazione facoltà
-if ($dbh->deleteFacolta($idFacolta)) {
-    header("Location: admin.php?action=facolta&success=deleted");
-} else {
-    header("Location: admin.php?action=facolta&error=delete_failed");
+    $idFacolta = intval($_POST['idfacolta']);
+
+    if ($dbh->deleteFacolta($idFacolta)) {
+        header("Location: admin.php?action=facolta&success=deleted");
+    } else {
+        header("Location: admin.php?action=facolta&error=delete_failed");
+    }
+    exit;
 }
 
+// Se nessuna azione valida è stata eseguita, reindirizza alla pagina delle facoltà
+header("Location: admin.php?action=facolta&error=invalid_action");
 exit;
-
-?>
