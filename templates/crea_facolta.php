be5fe4d599b30571eafbe33d6b0331f741fd660e
Maddalena Prandini <maddalena.prandini@studio.unibo.it>
2025-12-30 16:33:04 +0100

    first draft of create_button

---
+++ templates/crea_facolta.php
@@ -0,0 +1,10 @@
+<form action="">
+    <input type="text" name="nome_facolta" id="Nome Facoltà">Nomre Facoltà: </input>
+    <label for="scelta_anno">Scegli un'opzione: </label>
+    <select name="scelta_anno" id="scelta_anno">
+        <option value="">--Seleziona--</option>
+        <option value="triennale">triennale</option>
+        <option value="magistrale">magistrale</option>
+    </select>
+    <input type="submit" name="submit">Crea Facoltà</input>
+</form>
